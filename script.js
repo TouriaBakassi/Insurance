087aea80911aa52e5156df76bc28c487e18ffb21
TouiaBakassi <Touria_Bakassi@outlook.com>
2023-01-22 01:32:34 +0100

    The sequel

---
+++ script.js
@@ -0,0 +1,9 @@
+// let links = document.querySelectorAll('nav ul li a');
+// links.forEach((item)=>{
+//  item.addEventListener('click',()=>{
+//   links.forEach((link)=>{
+//     link.classList.remove('active');
+// });
+// item.classList.add('active');
+// });
+// });
