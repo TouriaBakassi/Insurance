e71d4a9d4e7330014fd706e412ce13d7f37e6d8a
TouiaBakassi <Touria_Bakassi@outlook.com>
2023-07-31 01:29:05 +0100

    The sequel

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// .............. Bar Button
+let barbutton= document.querySelector(".fa-bars");
+let navbar= document.getElementById("navbar");
+let xmark = document.querySelector(".fa-xmark");
+
+barbutton.addEventListener("click",()=>{
+  barbutton.classList.add("none");
+  xmark.classList.add("block");
+  navbar.classList.add("open");
+});
+
+xmark.addEventListener("click",()=>{
+  barbutton.classList.remove("none");
+  xmark.classList.remove("block")
+  navbar.classList.remove("open");
+});
+
+
 // .........................Contact Form
 
 let form = document.querySelector("#form");
 form.addEventListener("submit",()=>{
   alert("Your Message Is Done Successfully ✔💌");
 });
+
